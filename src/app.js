0ca58e4d65cd61474b465415bd0bce20ac9e28b2
wilianglo <will.d.joyboy@gmail.com>
2024-01-03 16:59:27 +0700

    tambah cart

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-document.addEventListener('Alpine:init', () => {
+document.addEventListener('alpine:init', () => {
     Alpine.data('products', () => ({
         items: [
-            { id: 1, name: 'Robusta Brazil', img: '1.img', price: 20000 },
-            { id: 2, name: 'Arabica Blend', img: '2.img', price: 25000 },
-            { id: 3, name: 'Primo Passo', img: '3.img', price: 30000 },
-            { id: 4, name: 'Aceh Gayo', img: '4.img', price: 35000 },
-            { id: 5, name: 'Sumatra Mandheling', img: '5.img', price: 40000 },
+            { id: 1, name: 'Robusta Brazil', img: '1.jpg', price: 20000 },
+            { id: 2, name: 'Arabica Blend', img: '2.jpg', price: 25000 },
+            { id: 3, name: 'Primo Passo', img: '3.jpg', price: 30000 },
+            { id: 4, name: 'Aceh Gayo', img: '4.jpg', price: 35000 },
+            { id: 5, name: 'Sumatra Mandheling', img: '5.jpg', price: 40000 },
         ],
     }));
-})
+
+    Alpine.store('cart', {
+        items: [],
+        total: 0,
+        quantity: 0,
+        add(newItem) {
+            console.log(newItem);
+        },
+    });
+});
+//Konversi ke rupiah
+const rupiah = (number) => {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0,
+    }).format(number);
+};
